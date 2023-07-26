c4aaafc67f9fdbbc03a284ef4993e06f1d860afe
Jorge Luis <jorgealcantara5a@gmail.com>
2023-07-26 16:18:38 -0500

    Let's begin

---
+++ public/js/info.js
@@ -0,0 +1,12 @@
+const DetailsList = [
+    {
+        'icon':'bi-balloon-fill',
+        'tittle':'Nacimiento',
+        'detail': '9 Junio 1994'
+    },
+    {
+        'icon':'bi-mortarboard-fill',
+        'tittle':'Colegiatura CIP',
+        'detail': '309385'
+    }
+];
